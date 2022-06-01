082723498cb58e9e8a52eaa67bd27b7cb0fec6fc
GiselyKC <gikavalli@gmail.com>
2022-06-01 15:31:06 -0300

    feat: adicionado rotas

--- router/index.js
+++ router/index.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 const salesController = require('../controllers/salesController');
 const { validateName, validateQuantity } = require('../middlewares/productsMiddleware');
+const { validateProductId, validateQuantitySales } = require('../middlewares/salesMiddleware');
 
 router.get('/products', productsController.getAll);
 router.get('/products/:id', productsController.getById);
 router.get('/sales', salesController.getAll);
 router.get('/sales/:id', salesController.getById);
-console.log('oi')
 router.post('/products', validateName, validateQuantity);
+router.post('/sales', validateProductId, validateQuantitySales);
+router.put('/products/:id', validateName, validateQuantity);
+router.put('/sales/:id', validateProductId, validateQuantitySales);
 module.exports = router;
