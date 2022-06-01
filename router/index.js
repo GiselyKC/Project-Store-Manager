6d3badb53a6035d44c0e9f24e80a256659d15062
GiselyKC <gikavalli@gmail.com>
2022-05-31 23:15:44 -0300

    test

--- router/index.js
+++ router/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 const salesController = require('../controllers/salesController');
+const { validateName, validateQuantity } = require('../middlewares/productsMiddleware');
 
 router.get('/products', productsController.getAll);
 router.get('/products/:id', productsController.getById);
 router.get('/sales', salesController.getAll);
 router.get('/sales/:id', salesController.getById);
-
+console.log('oi')
+router.post('/products', validateName, validateQuantity);
 module.exports = router;
