a0678328098bce833238c816114f48b069244bd5
GiselyKC <gikavalli@gmail.com>
2022-06-01 17:49:31 -0300

    feat: adicionado service da rota post /product

--- services/productsService.js
+++ services/productsService.js
@@ -1,9 +1,16 @@
 const productsModel = require('../models/productsModel');
 
 const getAll = () => productsModel.getAllProducts();
+
+const getInfos = (name) => productsModel.getInfosProducts(name);
+
 const getById = (id) => productsModel.getByIdProducts(id);
+
+const postAdd = (name, quantity) => productsModel.postAddProducts(name, quantity);
 
 module.exports = {
   getAll,
+  getInfos,
   getById,
+  postAdd,
 };
