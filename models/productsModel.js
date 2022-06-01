3a1acbc04d6a10098e1bd3ec60b63c5a5d8bfd46
GiselyKC <gikavalli@gmail.com>
2022-06-01 17:48:57 -0300

    feat: adicionado funções para a rota post /product

--- models/productsModel.js
+++ models/productsModel.js
@@ -4,11 +4,32 @@ const getAllProducts = () => connection.execute(
   'SELECT * FROM StoreManager.products',
 );
 
+const getInfosProducts = (name) => connection.execute(
+  'SELECT * FROM StoreManager.products WHERE StoreManager.products.name = ?',
+  [name],
+);
+
 const getByIdProducts = (id) => connection.execute(
   'SELECT * FROM StoreManager.products WHERE id = ?', [id],
 );
 
+const postAddProducts = async (name, quantity) => {
+  const [row] = await connection.execute(
+    `INSERT INTO StoreManager.products (name, quantity)
+    VALUES (?, ?)`,
+    [name, quantity],
+  );
+  const result = {
+    id: row.insertId,
+    name,
+    quantity,
+  };
+  return result;
+};
+
 module.exports = {
   getAllProducts,
+  getInfosProducts,
   getByIdProducts,
+  postAddProducts,
 };
