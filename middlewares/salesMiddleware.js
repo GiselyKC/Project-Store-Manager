8236cbdd5a7aa38803dda888db8243025750a612
GiselyKC <gikavalli@gmail.com>
2022-06-01 15:30:49 -0300

    feat: criado arq. salesMiddlewares.js

---
+++ middlewares/salesMiddleware.js
@@ -0,0 +1,39 @@
+const validateProductId = (req, res, next) => {
+  console.log('validateProductId');
+  const ProductId = req.body.some(({ productId }) => {
+    if (!productId || productId === '') {
+      return true;
+    } return false;
+  });
+  if (ProductId) {
+    return res.status(400).json({ message: '"productId" is required' });
+  }
+  next();
+};
+
+const someQuantitySales = ({ quantity }) => {
+  if (quantity === undefined || quantity === '') {
+    return true;
+  } return false;
+};
+
+const validateQuantitySales = (req, res, next) => {
+  console.log('oi');
+  const QuantitySales = req.body.some(someQuantitySales);
+  const QuantitySales2 = req.body.some(({ quantity }) => {
+    if (quantity <= 0) {
+      return true;
+    } return false;
+  });
+  if (QuantitySales) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  } if (QuantitySales2) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+    next();
+};
+
+module.exports = {
+  validateProductId,
+  validateQuantitySales,
+};
