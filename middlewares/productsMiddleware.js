bcbfe567a630e8daacb94baeed1538b7a707ec56
GiselyKC <gikavalli@gmail.com>
2022-06-01 15:30:28 -0300

    feat: criado arq. productsMiddleware.js

--- middlewares/productsMiddleware.js
+++ middlewares/productsMiddleware.js
@@ -1,7 +1,7 @@
 const validateName = (req, res, next) => {
   const { name } = req.body;
   console.log('validateName');
-  if (!name || name === '') {
+  if (name === undefined || name === '') {
     return res.status(400).json({ message: '"name" is required' });
   } if (name.length < 5) {
     return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
@@ -12,7 +12,7 @@ const validateName = (req, res, next) => {
 const validateQuantity = (req, res, next) => {
   const { quantity } = req.body;
 
-  if (!quantity || quantity === '') {
+  if (quantity === undefined || quantity === '') {
     return res.status(400).json({ message: '"quantity" is required' });
   } if (quantity <= 0) {
     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
