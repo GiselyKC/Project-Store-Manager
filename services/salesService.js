80e4510d0cdacd180b3ec816059dfa97ed13f4e3
GiselyKC <gikavalli@gmail.com>
2022-06-02 18:10:41 -0300

    feat: adicionado a validação para o endpoint post

--- services/salesService.js
+++ services/salesService.js
@@ -1,9 +1,29 @@
 const salesModel = require('../models/salesModel');
 
 const getAll = () => salesModel.getAllSales();
+
 const getById = (id) => salesModel.getByIdSales(id);
+
+const postAdd = async (array) => {
+  const { id } = await salesModel.postAddSales();
+
+  const infos = array.map(({ productId, quantity }) => {
+    salesModel.postAddSalesProducts(id, productId, quantity);
+    const arr = {
+      productId,
+      quantity,
+    };
+    return arr;
+  });
+
+  return {
+    id,
+    itemsSold: infos,
+  };
+};
 
 module.exports = {
   getAll,
   getById,
+  postAdd,
 };
