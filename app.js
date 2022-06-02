e0b7ed94df9dd2203723619d1aaf0c341ab1096a
GiselyKC <gikavalli@gmail.com>
2022-06-01 22:20:59 -0300

    feat: app.js modificado

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+require('express-async-errors'); 
+const middlewareErrors = require('./middlewares/error');
 
 const app = express();
-const router = require('./router/index');
+const router = require('./router');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// o 'express.json' permite que a nossa API consiga enxergar o body com obj.json
 app.use(express.json());
 app.use('/', router);
+app.use(middlewareErrors);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
